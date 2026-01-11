746db85b0d7d33e9d40e59e79eae117e53609a89
oldman <oldm2563@email.com>
2026-01-11 01:22:06 +0100

    create a file that includes all Classes

---
+++ services/require.php
@@ -0,0 +1,10 @@
+<?php
+    require __DIR__ . "/../core/Db.php";
+    require __DIR__ . "/../entities/User.php";
+    require __DIR__ . "/../entities/Member.php";
+    require __DIR__ . "/../entities/Admin.php";
+    require __DIR__ . "/../entities/ProjectChef.php";
+    require __DIR__ . "/../entities/Project.php";
+    require __DIR__ . "/../entities/Sprint.php";
+    require __DIR__ . "/../entities/Task.php";
+    require __DIR__ . "/../entities/Comment.php";
